eeae98e8ba86d46abeefef9d26b7902439303198
Marcio Bueno <marciobueno@gmail.com>
2026-02-20 23:10:08 +0000

    adicionando componente view + flexbox

--- expo-01/app/index.tsx
+++ expo-01/app/index.tsx
@@ -5,11 +5,14 @@ export default function Index() {
     <View
       style={{
         flex: 1,
+        flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
       <Text>Olá, Turma!</Text>
+      <View style={{ height: 100, backgroundColor: "blue", flex: 0.1 }} />
+      <View style={{ height: 100, backgroundColor: "red", flex: 0.4 }} />
     </View>
   );
 }
